22217b49668d8d621be2dfa6fa7c6734260ec125
Ayén Bonsoa <bonsoa.yen@gmail.com>
2023-01-01 20:18:52 +0000

    Rename Home to Index and update related informations

--- src/commons/en/Index.tsx
+++ src/commons/en/Index.tsx
@@ -1,9 +1,9 @@
 /**
 * @license Community | if opensource write "Community" | if for company write the company name like "Paypal"
-* @author You Name
+* @author Your Name
 *
-* Home.tsx 
-* This file retrieve all static string on { home } page
+* Index.tsx 
+* This file retrieve all static string on { index } page
 *
 * This source code is licensed under the { MIT } license found in the
 * LICENSE file in the root directory of this source tree.
